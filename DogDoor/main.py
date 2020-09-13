5958b108686a4e1e2231657bf11dac6b936a39f7
Jeff Tipps <jeff.tipps@ni.com>
2020-09-13 15:39:48 -0500

    added initial implementation and class diagram

---
+++ DogDoor/main.py
@@ -0,0 +1,42 @@
+class DogDoor:
+    _open: bool
+
+    def __init__(self):
+        self._open = False
+
+    def open(self) -> None:
+        print("The dog door opens.")
+        self._open = True
+
+    def close(self) -> None:
+        print("The dog door closes.")
+        self._open = False
+
+    def isOpen(self) -> bool:
+        return self._open
+
+
+class Remote:
+    _door: DogDoor
+
+    def __init__(self, door: DogDoor):
+        self._door = door
+
+    def press_button(self) -> None:
+        print("Pressing the remote control button...")
+        if self._door.isOpen():
+            self._door.close()
+        else:
+            self._door.open()
+
+
+door: DogDoor = DogDoor()
+remote: Remote = Remote(door)
+print("Fido barks to go outside...")
+remote.press_button()
+print("\nFido has gone outside")
+remote.press_button()
+print("\nFido's all done")
+remote.press_button()
+print("\nFido's back inside")
+remote.press_button()
